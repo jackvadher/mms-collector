dc071706da23dc1ffecfaf55d2fb44b567a5cf5b
jaimin.vadher <jaimin.vadher@aavidtechnologies.com>
2018-12-29 16:03:36 +0530

    First commit

---
+++ database/migrations/2014_10_12_000000_create_users_table_1.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMmsLogTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('mms-log', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->string('sid');
+            $table->string('from');
+            $table->string('to');
+            $table->text('body');
+            $table->integer('numMedia');
+            $table->string('status');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('mms-log');
+    }
+
+}
